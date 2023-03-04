2e1a67be673906fb690acfe165be2f82c6833131
HardDriver <ormo@mail.ru>
2023-03-05 01:25:18 +0300

    Показываем кубы чисел до введенного

---
+++ СSSeminar3/homework23/Program.cs
@@ -0,0 +1,46 @@
+﻿// Показываем кубы чисел до введенного
+Console.Clear();
+int userNum = num("Введите целое число и я покажу все кубы до него: ");
+if (userNum == 0) Console.WriteLine("У нуля любая степень равна нулю!");
+else
+{
+    Console.Write($"Это все кубы числа до числа {userNum}: ");
+
+    //считаем кубы
+    int a = 1;
+    if (userNum > 0)
+    {
+        while (a <= userNum)
+        {
+            Console.Write(Math.Pow(a, 3) + " ");
+            a++;
+        }
+    }
+    else
+    {
+        a = -1;
+        while (a >= userNum)
+        {
+            Console.Write(Math.Pow(a, 3) + " ");
+            a--;
+        }
+    }
+}
+
+//проверка ввода
+static int num(string message)
+{
+    while (true)
+    {
+        try
+        {
+            Console.Write(message);
+            return (int.Parse(Console.ReadLine() ?? ""));
+        }
+        catch (Exception exc)
+        {
+            Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+
+        }
+    }
+}
