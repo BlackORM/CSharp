07bde68a9e621b7b2b9f009a3284fc7e178764c7
HardDriver <ormo@mail.ru>
2023-03-09 00:26:46 +0300

    Заполнить массив из 8 рандомно 0 и 1 v.1.1

---
+++ СSSeminar4/test3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Заполнить массив из 8 рандомно 0 и 1
+int[] array = new int[8]; 
+FillArray(array); 
+//PrintArray(array); 
+Console.WriteLine($"[{String.Join(",", array)}]"); 
+ 
+void FillArray(int[] array) 
+{ 
+    int length = array.Length; 
+    for (int i = 0; i < length; i++) 
+    { 
+        array[i] = new Random().Next(0, 2); 
+    } 
+} 
+ 
+//void PrintArray(int[] array) 
+//{ 
+//    int length = array.Length; 
+//    Console.Write("["); 
+//    for (int i = 0; i < length; i++) 
+//    { 
+//        Console.Write($"{array[i]}"); 
+//        if (i < length - 1) Console.Write(", "); 
+//    } 
+//    Console.Write("]"); 
+//}
