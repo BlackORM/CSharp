6ece7a47cb3b4d0152c80f56e324a7d25201dd96
HardDriver <ormo@mail.ru>
2023-03-12 17:28:33 +0300

    сколько элементов попадает в отрезок

---
+++ СSSeminar5/test3/Program.cs
@@ -0,0 +1,43 @@
+﻿// Создаем рандомный массив и определяем сколько элементов попадает в отрезок [10, 99]
+Console.Clear();
+int num = InputNumber("Введите длинну массива: ", "Ошибка ввода данных!");
+int[] array = GetArray(num, 0, 199);
+Console.WriteLine(string.Join(" ,", array));
+int CountElements = Count(array);
+Console.WriteLine($"Количество элементов из отрезка 10-99 = {CountElements}");
+
+
+
+
+static int InputNumber(string message, string errormessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errormessage);
+    }
+}
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+int Count(int[] arr)
+{
+    int count = 0;
+    foreach (int item in arr)
+    {
+        if (item >= 10 && item < 100) count += 1;
+    }
+    return count;
+}
