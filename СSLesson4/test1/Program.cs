34d39fb34a3fa7852a826224dcdd42e923891b06
HardDriver <ormo@mail.ru>
2023-03-13 01:46:53 +0300

    новый двумерный массив

---
+++ СSLesson4/test1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Создание двумерного массива
+Console.WriteLine("Создаем двумерный массив");
+string[,] table = new string[2, 3];
+
+for (int i = 0; i < table.GetLength(0); i++)
+{
+    for (int j = 0; j < table.GetLength(1); j++)
+    {
+        Console.Write("0 ");
+    }
+    Console.WriteLine();
+}
+
