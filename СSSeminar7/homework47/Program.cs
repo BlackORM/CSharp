34ddb0bba4ae51926927a30f1c57596756310b36
HardDriver <ormo@mail.ru>
2023-03-19 22:13:23 +0300

    Двумерный массив заполненный случайными вещественными числами и округление

---
+++ СSSeminar7/homework47/Program.cs
@@ -0,0 +1,78 @@
+﻿// Двумерный массив заполненный случайными целыми
+
+Console.Clear();
+int row = InputPositiveNumber("Введите количество строк массива: ", "Ошибка ввода данных!");
+int col = InputPositiveNumber("Введите количество столбцов массива: ", "Ошибка ввода данных!");
+int minValue = InputNumber("Введите начало диапазона случайных чисел: ", "Ошибка ввода данных!");
+int maxValue = InputNumber("Введите конец диапазона случайных чисел: ", "Ошибка ввода данных!");
+//создаем двумерный массив случайных вещественных чисел
+double[,] array = GetDoubleArray(row, col, minValue, maxValue);
+//выводим массив 
+PrintArray(array);
+
+Console.Write("Введите количество знаков после запятой: ");
+int numDigits = InputPositiveNumber("Введите количество знаков после запятой: ", "Ошибка ввода данных!");
+//выводим массив с округлением до заданного количества цифр после запятой
+PrintRoundArray(array, numDigits);
+
+//ввод и проверка ввода любых чисел
+int InputNumber(string message, string errormessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errormessage);
+    }
+}
+//ввод и проверка ввода положительных чисел для строк и столбцов
+int InputPositiveNumber(string message, string errormessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect && userNumber > 0)
+            return userNumber;
+        Console.WriteLine(errormessage);
+    }
+}
+//генерация массива
+double[,] GetDoubleArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().NextDouble() * (maxValue - minValue) + minValue;
+        }
+    }
+    return result;
+}
+//вывод массива
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+//вывод массива с округлением
+void PrintRoundArray(double[,] inArray, int digits)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{Math.Round(inArray[i, j], digits)} ");
+        }
+        Console.WriteLine();
+    }
+}
