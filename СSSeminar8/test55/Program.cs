be2531869deaaf981ad5e45a89226358a1aa4221
HardDriver <ormo@mail.ru>
2023-03-20 19:52:43 +0300

    меняем строки и столбцы в массиве местами

---
+++ СSSeminar8/test55/Program.cs
@@ -0,0 +1,93 @@
+﻿// Меняем строки и столбцы местами
+
+Console.Clear();
+int row = InputPositiveNumber("Введите количество строк массива: ", "Ошибка ввода данных!");
+int col = InputPositiveNumber("Введите количество столбцов массива: ", "Ошибка ввода данных!");
+int minValue = InputNumber("Введите начало диапазона случайных чисел: ", "Ошибка ввода данных!");
+int maxValue = InputNumber("Введите конец диапазона случайных чисел: ", "Ошибка ввода данных!");
+
+//создаем массив
+int[,] array = GetArray(row, col, minValue, maxValue);
+PrintArray(array);
+//TransArray(array);
+int[,] transposeArray = new int[array.GetLength(1), array.GetLength(0)];
+for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            transposeArray[j, i] = array[i, j];
+        }
+    }
+
+
+
+Console.WriteLine();
+PrintArray(transposeArray);
+
+//ввод и проверка ввода любых чисел
+int InputNumber(string message, string errormessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errormessage);
+    }
+}
+
+//ввод и проверка ввода положительных чисел для строк и столбцов
+int InputPositiveNumber(string message, string errormessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect && userNumber > 0)
+            return userNumber;
+        Console.WriteLine(errormessage);
+    }
+}
+
+//генерация массива
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+//вывод массива
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+// int[,] TransArray(int[,] inArray);
+// {
+//     int[,] transposeArray = new int[inArray.GetLength(1), inArray.GetLength(0)];
+//     for (int i = 0; i < inArray.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < inArray.GetLength(1); j++)
+//         {
+//             transposeArray[i, j] = array[j, i];
+//         }
+//     }
+//     return transposeArray;
+// }
