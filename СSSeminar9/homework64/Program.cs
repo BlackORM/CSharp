5057c9fc38c86703dcd849819fb7ac7ea7fc782a
HardDriver <ormo@mail.ru>
2023-03-26 18:03:56 +0300

    Вывод всех натуральных чисел между двумя заданными в обратном порядке (через рекурсию)

---
+++ СSSeminar9/homework64/Program.cs
@@ -0,0 +1,35 @@
+﻿// Вывод всех натуральных чисел между двумя заданными в обратном порядке (через рекурсию)
+
+Console.Clear();
+int M = InputNumber("Введите первое число: ", "Ошибка ввода данных!");
+int N = InputNumber("Введите второе число: ", "Ошибка ввода данных!");
+
+if (M > N)
+{
+    int temp = M;
+    M = N;
+    N = temp;
+}
+
+string result = NumbersFromMtoN(M, N);
+Console.WriteLine($"Натуральные числа от {M} до {N} в обратном порядке: {result}");
+
+string NumbersFromMtoN(int m, int n)
+{
+    if (m == n)
+        return m.ToString();
+    else return n + ", " + NumbersFromMtoN(m, n-1);
+}
+
+//ввод и проверка ввода любых чисел
+int InputNumber(string message, string errormessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errormessage);
+    }
+}
