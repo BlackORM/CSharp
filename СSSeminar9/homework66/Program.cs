82560698f4a659bbb8e4794f5279d80160ea922b
HardDriver <ormo@mail.ru>
2023-03-26 18:12:02 +0300

    Сумма натуральных чисел между двумя заданными (через рекурсию)

---
+++ СSSeminar9/homework66/Program.cs
@@ -0,0 +1,35 @@
+﻿// Сумма натуральных чисел между двумя заданными (через рекурсию)
+
+Console.Clear();
+
+int M = InputNumber("Введите первое число: ", "Ошибка ввода данных!");
+int N = InputNumber("Введите второе число: ", "Ошибка ввода данных!");
+
+if (M > N)
+{
+    int temp = M;
+    M = N;
+    N = temp;
+}
+
+int result = SumFromMtoN(M, N);
+Console.WriteLine($"Сумма чисел между {M} и {N}: {result}");
+
+int SumFromMtoN(int m, int n)
+{
+    if (m == n) return m;
+    return m + SumFromMtoN(m + 1, n);
+}
+
+//ввод и проверка ввода любых чисел
+int InputNumber(string message, string errormessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect)
+            return userNumber;
+        Console.WriteLine(errormessage);
+    }
+}
