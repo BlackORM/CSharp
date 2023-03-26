696735884d8ba91ba38b507105283f2023064fc5
HardDriver <ormo@mail.ru>
2023-03-26 17:11:19 +0300

    ывод всех натуральных чисел между двумя заданными

---
+++ СSSeminar9/test65/Program.cs
@@ -0,0 +1,33 @@
+﻿// Вывод всех натуральных чисел между двумя заданными
+
+Console.Clear();
+
+int M = InputPositiveNumber("Введите первое число: ", "Ошибка ввода данных!");
+int N = InputPositiveNumber("Введите второе число: ", "Ошибка ввода данных!");
+
+
+string result = NumbersFromMtoN(M, N);
+
+Console.WriteLine($"Натуральные числа от M={M} до N={N}: {result}");
+
+
+string NumbersFromMtoN(int m, int n)
+{
+    if (m == n)
+        return m.ToString();
+    else return m + ", " + NumbersFromMtoN(m + 1, n);
+}
+
+//ввод и проверка ввода положительных чисел для строк и столбцов
+int InputPositiveNumber(string message, string errormessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        bool isCorrect = int.TryParse(Console.ReadLine(), out int userNumber);
+        if (isCorrect && userNumber > 0)
+            return userNumber;
+        Console.WriteLine(errormessage);
+    }
+}
+
